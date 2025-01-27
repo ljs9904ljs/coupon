e41d8b498690af9bc4b2ced3e9e426da7a17d1c9
ljs9904ljs <57557223+ljs9904ljs@users.noreply.github.com>
2025-01-27 20:20:35 +0900

    카프카 consumer 안에서 신규 메세지를 produce하는데, 그와 관련된 로그를 남김.

--- src/main/kotlin/com/example/coupon/kafka/KafkaConsumer.kt
+++ src/main/kotlin/com/example/coupon/kafka/KafkaConsumer.kt
@@ -4,6 +4,7 @@ import com.example.coupon.redisonly.IssueCouponReq
 import com.example.coupon.redisonly.RedisOnlyCouponService
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
+import org.springframework.context.annotation.DependsOn
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
@@ -17,9 +18,11 @@ class KafkaConsumer(
 
     @KafkaListener(topics = ["mytopic"])
     fun listen(message: String) {
-        val result = redisOnlyCouponService.issueCoupon(IssueCouponReq(message))
+        val result = redisOnlyCouponService.issueCoupon(IssueCouponReq(id = message))
         val jsonStr = objectMapper.writeValueAsString(result)
-        kafkaProducer.send("response-topic", jsonStr)
-        println("msg received. code: ${result.code}, num: ${result.num}")
+        val kafkaResult = kafkaProducer.send("response-topic", jsonStr)
+        val completed = kafkaResult.join()
+
+        println("msg received. completed($completed)    <<$jsonStr>>    <<$result>>")
     }
 }
--- src/main/kotlin/com/example/coupon/kafka/KafkaProducer.kt
+++ src/main/kotlin/com/example/coupon/kafka/KafkaProducer.kt
@@ -1,7 +1,9 @@
 package com.example.coupon.kafka
 
 import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.SendResult
 import org.springframework.stereotype.Component
+import java.util.concurrent.CompletableFuture
 
 
 @Component
@@ -9,8 +11,8 @@ class KafkaProducer(
     private val kafkaTemplate: KafkaTemplate<String, String>
 ) {
 
-    fun send(topic: String, message: String) {
-        kafkaTemplate.send(topic, message)
+    fun send(topic: String, message: String): CompletableFuture<SendResult<String, String>> {
+        return kafkaTemplate.send(topic, message)
     }
 
 }
