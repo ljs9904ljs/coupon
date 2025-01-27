26b17663ea73c98217abb768797173ffe1eb7128
ljs9904ljs <57557223+ljs9904ljs@users.noreply.github.com>
2025-01-27 20:21:06 +0900

    해당 client id 값을 이용해 추후 Server-Sent-Event를 전송함.

--- src/main/kotlin/com/example/coupon/redisonly/IssueCouponRes.kt
+++ src/main/kotlin/com/example/coupon/redisonly/IssueCouponRes.kt
@@ -1,10 +1,13 @@
 package com.example.coupon.redisonly
 
-class IssueCouponRes(
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class IssueCouponRes(
     // response code.
     // 0: 성공
     // 1: 더블 클릭
     // -1: 실패
     val code: Int,
+    val clientId: String? = null,
     val num: Int? = null
 )
